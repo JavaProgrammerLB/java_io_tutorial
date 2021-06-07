4a203a492a10e809aa3c6f2ef8031e44ce442d20
刘备 <admin@yitianyigexiangfa.com>
2021-06-07 16:32:23 +0800

    【bind】ServerSocketChannel的bind方法

---
+++ java_nio_tutorial/src/main/java/liubei/java/nio/tutorial/nio_example_01/Server2.java
@@ -0,0 +1,38 @@
+package liubei.java.nio.tutorial.nio_example_01;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.channels.ServerSocketChannel;
+
+public class Server2 {
+
+    public static void main(String[] args) {
+        try {
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.bind(new InetSocketAddress("localhost", 8888));
+            ServerSocket serverSocket = serverSocketChannel.socket();
+            Socket socket = serverSocket.accept();
+            InputStream inputStream = socket.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            char[] charArray = new char[1024];
+            int readableLength = inputStreamReader.read(charArray);
+            while (readableLength != -1) {
+                String str = new String(charArray, 0, readableLength);
+                System.out.println(str);
+                readableLength = inputStreamReader.read(charArray);
+            }
+            inputStreamReader.close();
+            inputStream.close();
+            socket.close();
+            serverSocket.close();
+            serverSocketChannel.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
