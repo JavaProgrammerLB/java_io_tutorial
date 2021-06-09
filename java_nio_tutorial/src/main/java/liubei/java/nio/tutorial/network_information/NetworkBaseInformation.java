6dcb99681bebab20c991d9e6bbb45b17512e2a78
刘备 <admin@yitianyigexiangfa.com>
2021-06-09 17:04:23 +0800

    【网络接口】基本信息

---
+++ java_nio_tutorial/src/main/java/liubei/java/nio/tutorial/network_information/NetworkBaseInformation.java
@@ -0,0 +1,27 @@
+package liubei.java.nio.tutorial.network_information;
+
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public class NetworkBaseInformation {
+
+    public static void main(String[] args) {
+        try {
+            Enumeration<NetworkInterface> networkInterfaceEnumeration = NetworkInterface.getNetworkInterfaces();
+            while (networkInterfaceEnumeration.hasMoreElements()) {
+                NetworkInterface networkInterface = networkInterfaceEnumeration.nextElement();
+                System.out.println("getName获取结果为：" + networkInterface.getName());
+                System.out.println("getDisplayName获取结果为：" + networkInterface.getDisplayName());
+                System.out.println("getIndex获取结果为：" + networkInterface.getIndex());
+                System.out.println("是否已经开启: " + networkInterface.isUp());
+                System.out.println("是否回环: " + networkInterface.isLoopback());
+                System.out.println();
+                System.out.println();
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
